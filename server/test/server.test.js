c35b9c8847bdcba702020e402bedc84c95a7311d
hector <hectorgarcia849@gmail.com>
2017-09-11 14:56:07 -0400

    Change format of http response for USER calls

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -466,8 +466,8 @@ describe('POST /users/', () => {
             .expect(200)
             .expect((res) => {
                 expect(res.headers['x-auth']).toBeTruthy();
-                expect(res.body._id).toBeTruthy();
-                expect(res.body.email).toBe(email);})
+                expect(res.body.user._id).toBeTruthy();
+                expect(res.body.user.email).toBe(email);})
             .end((err) => {
                 if(err){
                     return done(err);
